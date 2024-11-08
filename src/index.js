266a4c72f6102e50956e444541d6c010333c3c19
Ivan Lartsov <lartsov@gmail.com>
2024-11-08 22:24:47 +0300

    upd: libs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
 
 import "normalize.css";
-import "./index.scss";
+import "./styles/index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-);
+import App from "./components/App.js";
+
+ReactDOM.createRoot(document.getElementById("root"))
+	.render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	);
